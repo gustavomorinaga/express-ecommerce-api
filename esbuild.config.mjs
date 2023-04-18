f51b01ebc8d9e23dc1aa3ceb4769f14e8e230d93
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-04-18 19:52:21 -0300

    fix: top level async esbuild config

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,16 +1,18 @@
 import { build } from 'esbuild';
 import esbuildPluginPino from 'esbuild-plugin-pino';
 
-await build({
-	entryPoints: ['src/index.ts'],
-	bundle: true,
-	minify: true,
-	platform: 'node',
-	target: 'node18',
-	outdir: 'dist',
-	plugins: [
-		esbuildPluginPino({
-			transports: ['pino-pretty'],
-		}),
-	],
-});
+(async () => {
+	await build({
+		entryPoints: ['src/index.ts'],
+		bundle: true,
+		minify: true,
+		platform: 'node',
+		target: 'node18',
+		outdir: 'dist',
+		plugins: [
+			esbuildPluginPino({
+				transports: ['pino-pretty'],
+			}),
+		],
+	});
+})();
