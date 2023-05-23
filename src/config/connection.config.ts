8d6ed46ebd61b0ec972d1ff39aee47b58edda46e
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-05-23 04:41:41 -0300

    fix: removing unused old mongo env variables validation

--- src/config/connection.config.ts
+++ src/config/connection.config.ts
@@ -19,11 +19,6 @@ export const connect = async () => {
 			if (!(environment as any)[variable])
 				return handleError(`${variable} not found`, 'INTERNAL_SERVER_ERROR');
 
-		if (!environment.DATABASE_USER) throw new Error('DATABASE_USER not found');
-		if (!environment.DATABASE_PASS) throw new Error('DATABASE_PASS not found');
-		if (!environment.DATABASE_HOST) throw new Error('DATABASE_HOST not found');
-		if (!environment.DATABASE_NAME) throw new Error('DATABASE_NAME not found');
-
 		const uri = `mongodb+srv://${environment.DATABASE_USER}:${environment.DATABASE_PASS}@${environment.DATABASE_HOST}/${environment.DATABASE_NAME}?retryWrites=true&w=majority`;
 
 		mongoose.connection.on('connecting', () => {
